77ccab585dcbe5e9c6c8a17e588095bef2e302b3
Jorge Henrique Silva Saldanha <jorgehenriquesaldanha@gmail.com>
2023-11-02 16:39:52 -0300

    Equipe, excluir e adicionar barbeiro

--- src/pages/Administrador/AdministradorPage.tsx
+++ src/pages/Administrador/AdministradorPage.tsx
@@ -1,67 +1,54 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, ChangeEvent } from "react";
 import axios from "axios";
-import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
-import { AgendamentoBox, AgendamentoContainer } from "./AdministradorPageStyle";
-import { Agendamento } from "../../types/types";
+import { toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 const AdministradorPage = () => {
-  const [nomeUsuario, setNomeUsuario] = useState("");
+  const [nomeAdministrador, setNomeAdministrador] = useState("");
   const [barbeirosData, setBarbeirosData] = useState<any[]>([]);
   const [barbeiroIdSelecionado, setBarbeiroIdSelecionado] = useState<
     number | null
   >(null);
-  const [servicosSelecionados, setServicosSelecionados] = useState<string[]>(
-    []
-  );
-  const [dataSelecionada, setDataSelecionada] = useState<Date | null>(null);
-  const [formaPagamento, setFormaPagamento] = useState<string | null>(null);
-  const [formaPagamentoSelecionada, setFormaPagamentoSelecionada] = useState<
-    string | null
-  >(null);
-
-  const [horarioInicio, setHorarioInicio] = useState<Date | null>(null);
-  const [horarioFinal, setHorarioFinal] = useState<Date | null>(null);
   const [nomeBarbeiroSelecionado, setNomeBarbeiroSelecionado] =
     useState<string>("");
-  const [valorTotal, setValorTotal] = useState<number>(0);
-  const [agendamentos, setAgendamentos] = useState<Agendamento[]>([]);
-  const [mostrarAgendamentos, setMostrarAgendamentos] = useState(false);
-  const [resumoFinanceiro, setResumoFinanceiro] = useState({
-    quantidadeCortes: 0,
-    valorTotal: 0,
-    mediaPorCorte: 0,
-    formasPagamento: 0,
+  const [exibirFormulario, setExibirFormulario] = useState(false);
+  const [novoBarbeiro, setNovoBarbeiro] = useState({
+    nome: "",
+    fotoperfil: "",
+    email: "",
+    telefone: "",
+    senha: "",
+    sobre: "",
+    fotos_trabalhos: [""],
   });
-  const [formasPagamento, setFormasPagamento] = useState<{
-    [forma: string]: number;
-  }>({});
 
   useEffect(() => {
-    const userId = localStorage.getItem("userId");
+    const administradorId = localStorage.getItem("administradorId");
 
-    if (userId) {
+    if (administradorId) {
       axios
-        .get(`http://localhost:3001/usuarios/${userId}`)
+        .get(`http://localhost:3001/administradores/${administradorId}`)
         .then((response) => {
-          const nomeDoUsuario = response.data.nome;
+          const nomeDoAdministrador = response.data.nome;
           const nomeCapitalizado =
-            nomeDoUsuario.charAt(0).toUpperCase() + nomeDoUsuario.slice(1);
-          setNomeUsuario(nomeCapitalizado);
+            nomeDoAdministrador.charAt(0).toUpperCase() +
+            nomeDoAdministrador.slice(1);
+          setNomeAdministrador(nomeCapitalizado);
         })
         .catch((error) => {
-          console.error("Erro ao buscar o nome do usuário:", error);
+          console.error("Erro ao buscar o nome do administrador:", error);
         });
     }
   }, []);
 
-  const handleSolicitarAgendamento = () => {
+  const handleEquipeClick = () => {
     axios
       .get("http://localhost:3001/barbeiros")
       .then((response) => {
         const barbeiros = response.data.map((barbeiro: any) => ({
           ...barbeiro,
-          selecionado: true, // Defina todos os barbeiros como selecionados inicialmente
+          selecionado: true,
         }));
         setBarbeirosData(barbeiros);
       })
@@ -71,7 +58,6 @@ const AdministradorPage = () => {
   };
 
   const handleSelecionarBarbeiro = (barbeiroId: number, nome: string) => {
-    // Crie uma cópia do estado de barbeirosData para evitar mutações diretas
     const novosBarbeirosData = [...barbeirosData];
 
     const barbeiroClicado = novosBarbeirosData.find(
@@ -93,187 +79,76 @@ const AdministradorPage = () => {
     }
   };
 
-  const handleServicoSelecionado = (servico: string) => {
-    if (servicosSelecionados.includes(servico)) {
-      // Se o serviço já estiver na lista de serviços selecionados, remova-o
-      setServicosSelecionados(
-        servicosSelecionados.filter((s) => s !== servico)
-      );
-      // Também limpe a data selecionada quando o serviço é desselecionado
-      setDataSelecionada(null);
-    } else {
-      // Caso contrário, adicione-o à lista de serviços selecionados
-      setServicosSelecionados([...servicosSelecionados, servico]);
-    }
+  const exibirFormularioAdicao = () => {
+    setExibirFormulario(true);
   };
 
-  useEffect(() => {
-    // Mapear os tempos estimados para cada serviço
-    const temposEstimados: { [key: string]: number } = {
-      barba: 30,
-      cabelo: 30,
-      sobrancelha: 15,
-    };
-
-    const duracaoTotal = servicosSelecionados.reduce(
-      (total, servico) => total + temposEstimados[servico],
-      0
-    );
-
-    if (
-      barbeiroIdSelecionado !== null &&
-      servicosSelecionados.length > 0 &&
-      dataSelecionada instanceof Date
-    ) {
-      const novaDataInicio = new Date(dataSelecionada.getTime());
-      const novaDataFinal = new Date(
-        dataSelecionada.getTime() + duracaoTotal * 60000
-      );
-      setHorarioInicio(novaDataInicio);
-      setHorarioFinal(novaDataFinal);
-    }
-  }, [servicosSelecionados, barbeiroIdSelecionado, dataSelecionada]);
-
-  const handleFormaPagamentoSelecionada = (forma: string) => {
-    setFormaPagamentoSelecionada(forma);
-
-    // Mapeie os preços dos serviços selecionados
-    const precosServicos: { [key: string]: number } = {
-      barba: 10,
-      cabelo: 30,
-      sobrancelha: 5,
-    };
-
-    const total = servicosSelecionados.reduce(
-      (total, servico) => total + precosServicos[servico],
-      0
-    );
-
-    setValorTotal(total);
+  const adicionarCampoFotosTrabalhos = () => {
+    const novasFotosTrabalhos = [...novoBarbeiro.fotos_trabalhos];
+    novasFotosTrabalhos.push("");
+    setNovoBarbeiro({ ...novoBarbeiro, fotos_trabalhos: novasFotosTrabalhos });
   };
 
-  const handleSalvarAgendamento = () => {
-    if (
-      barbeiroIdSelecionado === null ||
-      servicosSelecionados.length === 0 ||
-      dataSelecionada === null ||
-      formaPagamentoSelecionada === null ||
-      horarioInicio === null
-    ) {
-      alert("Preencha todos os campos obrigatórios.");
-      return;
-    }
+  const handleFotosTrabalhosChange = (
+    index: number,
+    event: ChangeEvent<HTMLInputElement>
+  ) => {
+    const novasFotosTrabalhos = [...novoBarbeiro.fotos_trabalhos];
+    novasFotosTrabalhos[index] = event.target.value;
+    setNovoBarbeiro({ ...novoBarbeiro, fotos_trabalhos: novasFotosTrabalhos });
+  };
 
-    const agendamento = {
-      cliente_id: localStorage.getItem("userId"),
-      barbeiro_id: barbeiroIdSelecionado,
-      hora: horarioInicio.toLocaleTimeString(),
-      data: dataSelecionada.toISOString(),
-      valor: valorTotal,
-      forma_pagamento: formaPagamentoSelecionada,
-      status: "aguardando",
-    };
-
-    console.log("Dados do Agendamento:", agendamento);
-
+  const enviarFormulario = () => {
     axios
-      .post("http://localhost:3001/agendamentos", agendamento)
+      .post("http://localhost:3001/adicionar-barbeiro", novoBarbeiro)
       .then((response) => {
-        alert("Agendamento salvo com sucesso!");
+        toast.success("Barbeiro adicionado com sucesso!");
+        setNovoBarbeiro({
+          nome: "",
+          fotoperfil: "",
+          email: "",
+          telefone: "",
+          senha: "",
+          sobre: "",
+          fotos_trabalhos: [""],
+        });
       })
       .catch((error) => {
-        console.error("Erro ao salvar agendamento:", error);
+        console.error("Erro ao adicionar barbeiro:", error);
+        toast.error("Erro ao adicionar barbeiro.");
       });
   };
 
-  const handleMostrarAgendamentos = () => {
-    const userId = localStorage.getItem("userId");
+  const handleSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+    enviarFormulario();
+  };
 
-    if (userId) {
+  const handleExcluirBarbeiro = (barbeiroId: number) => {
+    if (window.confirm("Tem certeza de que deseja excluir este barbeiro?")) {
       axios
-        .get(`http://localhost:3001/agendamentos/usuario/${userId}`)
+        .delete(`http://localhost:3001/barbeiros/${barbeiroId}`)
         .then((response) => {
-          setAgendamentos(response.data);
-          setMostrarAgendamentos(true);
+          toast.success("Barbeiro excluído com sucesso!");
+          // Atualize a lista de barbeiros após a exclusão
+          const barbeirosAtualizados = barbeirosData.filter(
+            (barbeiro) => barbeiro.idbarbeiro !== barbeiroId
+          );
+          setBarbeirosData(barbeirosAtualizados);
+          setBarbeiroIdSelecionado(null);
+          setNomeBarbeiroSelecionado("");
         })
         .catch((error) => {
-          console.error("Erro ao buscar os agendamentos:", error);
+          console.error("Erro ao excluir barbeiro:", error);
+          toast.error("Erro ao excluir barbeiro.");
         });
-    } else {
-      console.error("ID do usuário não encontrado no localStorage.");
-    }
-  };
-
-  const calcularResumoFinanceiro = (userId: string) => {
-    axios
-      .get(`http://localhost:3001/agendamentos/usuario/${userId}`)
-      .then((response) => {
-        const agendamentos = response.data;
-        const quantidadeCortes = agendamentos.length;
-
-        // Calcular o valor total
-        const valorTotal = agendamentos.reduce(
-          (total: number, agendamento: { valor: string }) =>
-            total + parseFloat(agendamento.valor),
-          0
-        );
-
-        // Calcular a média por corte
-        const mediaPorCorte =
-          quantidadeCortes > 0 ? valorTotal / quantidadeCortes : 0;
-
-        // Calcular as formas de pagamento e a contagem de uso
-        const formasPagamento: { [forma: string]: number } = {};
-        agendamentos.forEach((agendamento: { forma_pagamento: any }) => {
-          const formaPagamento = agendamento.forma_pagamento;
-          formasPagamento[formaPagamento] =
-            (formasPagamento[formaPagamento] || 0) + 1;
-        });
-
-        // Atualizar os estados
-        setResumoFinanceiro({
-          quantidadeCortes,
-          valorTotal,
-          mediaPorCorte,
-          formasPagamento,
-        });
-      })
-      .catch((error) => {
-        console.error("Erro ao buscar os agendamentos:", error);
-      });
-  };
-
-  const handleMostrarResumoFinanceiro = () => {
-    // Use o ID do usuário atual para buscar os agendamentos e calcular o resumo financeiro
-    const userId = localStorage.getItem("userId");
-    if (userId) {
-      calcularResumoFinanceiro(userId);
-    } else {
-      console.error("ID do usuário não encontrado no localStorage.");
     }
   };
 
   return (
     <div>
-      <h1 style={{ color: "black" }}>Olá, {nomeUsuario}</h1>
-      <button onClick={handleSolicitarAgendamento}>Solicitar aaaaaaaaaa</button>
-      <button onClick={handleMostrarAgendamentos}>Agendamentos</button>
-      <button onClick={handleMostrarResumoFinanceiro}>Resumo Financeiro</button>
-      {mostrarAgendamentos && (
-        <div>
-          <h2>Agendamentos</h2>
-          <AgendamentoContainer>
-            {agendamentos.map((agendamento) => (
-              <AgendamentoBox key={agendamento.id}>
-                <p>Barbeiro: {agendamento.barbeiro}</p>
-                <p>Data/Hora: {agendamento.data}</p>
-                <p>Valor Total: R$ {agendamento.valor}</p>
-                <p>Status: {agendamento.status}</p>
-              </AgendamentoBox>
-            ))}
-          </AgendamentoContainer>
-        </div>
-      )}
+      <h1 style={{ color: "black" }}>Olá, {nomeAdministrador}</h1>
+      <button onClick={handleEquipeClick}>Equipe</button>
       {barbeirosData.length > 0 && (
         <div>
           <h2>Barbeiros</h2>
@@ -310,117 +185,90 @@ const AdministradorPage = () => {
                   />
                 </div>
                 <p>{barbeiro.nome}</p>
+                <button
+                  onClick={() => handleExcluirBarbeiro(barbeiro.idbarbeiro)}
+                >
+                  Excluir
+                </button>
               </li>
             ))}
           </ul>
         </div>
       )}
-      {barbeiroIdSelecionado !== null && (
+      <button onClick={exibirFormularioAdicao}>Adicionar Barbeiro</button>
+      {exibirFormulario && (
         <div>
-          <h2>Serviços</h2>
-          <label>
+          <h2>Adicionar Barbeiro</h2>
+          <form onSubmit={handleSubmit}>
+            <label>Nome:</label>
             <input
-              type="checkbox"
-              value="barba"
-              checked={servicosSelecionados.includes("barba")}
-              onChange={() => handleServicoSelecionado("barba")}
+              type="text"
+              value={novoBarbeiro.nome}
+              onChange={(e) =>
+                setNovoBarbeiro({ ...novoBarbeiro, nome: e.target.value })
+              }
             />
-            Barba
-          </label>
-          <label>
+            <br />
+            <label>Foto de Perfil (URL):</label>
             <input
-              type="checkbox"
-              value="cabelo"
-              checked={servicosSelecionados.includes("cabelo")}
-              onChange={() => handleServicoSelecionado("cabelo")}
+              type="text"
+              value={novoBarbeiro.fotoperfil}
+              onChange={(e) =>
+                setNovoBarbeiro({ ...novoBarbeiro, fotoperfil: e.target.value })
+              }
             />
-            Cabelo
-          </label>
-          <label>
+            <br />
+            <label>Email:</label>
             <input
-              type="checkbox"
-              value="sobrancelha"
-              checked={servicosSelecionados.includes("sobrancelha")}
-              onChange={() => handleServicoSelecionado("sobrancelha")}
+              type="text"
+              value={novoBarbeiro.email}
+              onChange={(e) =>
+                setNovoBarbeiro({ ...novoBarbeiro, email: e.target.value })
+              }
             />
-            Sobrancelha
-          </label>
-        </div>
-      )}
-      {servicosSelecionados.length > 0 && (
-        <div>
-          <h2>Selecione a Data e Hora</h2>
-          <DatePicker
-            selected={dataSelecionada}
-            onChange={(date) => setDataSelecionada(date)}
-            showTimeSelect
-            timeFormat="HH:mm"
-            timeIntervals={15}
-            timeCaption="Hora"
-            dateFormat="dd/MM/yyyy HH:mm"
-            placeholderText="Selecione a data e hora"
-          />
-        </div>
-      )}
-      {dataSelecionada !== null && (
-        <div>
-          <h2>Forma de Pagamento</h2>
-          <label>
+            <br />
+            <label>Telefone:</label>
             <input
-              type="radio"
-              value="cartao"
-              checked={formaPagamento === "cartao"}
-              onChange={() => handleFormaPagamentoSelecionada("cartao")}
+              type="text"
+              value={novoBarbeiro.telefone}
+              onChange={(e) =>
+                setNovoBarbeiro({ ...novoBarbeiro, telefone: e.target.value })
+              }
             />
-            Cartão de Crédito/Débito
-          </label>
-          <label>
+            <br />
+            <label>Senha:</label>
             <input
-              type="radio"
-              value="pix"
-              checked={formaPagamento === "pix"}
-              onChange={() => handleFormaPagamentoSelecionada("pix")}
+              type="password"
+              value={novoBarbeiro.senha}
+              onChange={(e) =>
+                setNovoBarbeiro({ ...novoBarbeiro, senha: e.target.value })
+              }
             />
-            PIX
-          </label>
-          <label>
-            <input
-              type="radio"
-              value="dinheiro"
-              checked={formaPagamento === "dinheiro"}
-              onChange={() => handleFormaPagamentoSelecionada("dinheiro")}
+            <br />
+            <label>Sobre:</label>
+            <textarea
+              value={novoBarbeiro.sobre}
+              onChange={(e) =>
+                setNovoBarbeiro({ ...novoBarbeiro, sobre: e.target.value })
+              }
             />
-            Dinheiro
-          </label>
-        </div>
-      )}
-      {formaPagamentoSelecionada && (
-        <div>
-          <h2>Resumo do Agendamento</h2>
-          <p>Cliente: {nomeUsuario}</p>
-          <p>Barbeiro: {nomeBarbeiroSelecionado}</p>
-          <p>Serviços: {servicosSelecionados.join(", ")}</p>
-          <p>Data/Hora Início: {horarioInicio?.toLocaleString()}</p>
-          <p>Data/Hora Término: {horarioFinal?.toLocaleString()}</p>
-          <p>Forma de Pagamento: {formaPagamentoSelecionada}</p>
-          <p>Valor Total: R$ {valorTotal.toFixed(2)}</p>
-          <button onClick={handleSalvarAgendamento}>Salvar Agendamento</button>
-        </div>
-      )}
-      {resumoFinanceiro && (
-        <div>
-          <h2>Resumo Financeiro</h2>
-          <p>Quantidade de Cortes: {resumoFinanceiro.quantidadeCortes}</p>
-          <p>Valor Total: R$ {resumoFinanceiro.valorTotal.toFixed(2)}</p>
-          <p>Média por Corte: R$ {resumoFinanceiro.mediaPorCorte.toFixed(2)}</p>
-          <h3>Formas de Pagamento</h3>
-          <ul>
-            {Object.keys(resumoFinanceiro.formasPagamento).map((forma) => (
-              <li key={forma}>
-                {forma}: {resumoFinanceiro.formasPagamento[forma]}
-              </li>
+            <br />
+            <label>Fotos de Trabalhos (URL):</label>
+            {novoBarbeiro.fotos_trabalhos.map((foto, index) => (
+              <div key={index}>
+                <input
+                  type="text"
+                  value={foto}
+                  onChange={(e) => handleFotosTrabalhosChange(index, e)}
+                />
+              </div>
             ))}
-          </ul>
+            <button type="button" onClick={adicionarCampoFotosTrabalhos}>
+              +
+            </button>
+            <br />
+            <button type="submit">Adicionar Barbeiro</button>
+          </form>
         </div>
       )}
     </div>
